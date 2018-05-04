b3c06fd1a546c05941226ff60053142b2579b0c8
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-04 09:01:27 -0700

    added routes folder with appropriate files

---
+++ Routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import projectRoute from './projectRoute';
+import actionsRoute from './actionsRoute';
+
+const mainRouter = Router({ mergeParams: true });
+
+// projects
+mainRouter.use('/projects', projectRoute);
+
+// actions
+mainRouter.use('/actions', actionsRoute);
+
+export default mainRouter;
