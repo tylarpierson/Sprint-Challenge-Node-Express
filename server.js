574b33f8437ddf4ab2c2185c7b98eeccc905f6d2
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-04 09:12:37 -0700

    started routes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+import mainRouter from './Routes';
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('/api', mainRouter);
 
 app.listen(3000, () => console.log('hey'));
