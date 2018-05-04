a9a7a7c52701667e87c78fbf4dd0207978f6782f
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-04 10:53:03 -0700

    added actions routes

--- Routes/actionsRoute.js
+++ Routes/actionsRoute.js
@@ -4,3 +4,45 @@ const { validateBody, respondWithError } = require('../utils');
 const { NOT_FOUND_ERROR, INPUT_ERROR, REMOVE_ERROR, PUT_ERROR } = require('../Errors');
 
 const actionsRoute = express.Router();
+
+// gets
+actionsRoute.get('/', async (req, res) => {
+    try {
+        const actions = await db.get();
+        res.json(actions);
+    } catch (e) {
+        respondWithError(res);
+    };
+});
+
+actionsRoute.get('/:id', async (req, res) => {
+    try {
+        const action = await db.get(+req.params.id);
+        if (!action.notes) respondWithError(res, NOT_FOUND_ERROR);
+        res.json(action);
+    } catch (e) {
+        respondWithError(res);
+    };
+});
+
+// post
+actionsRoute.post('/', async (req, res) => {
+    try {
+        const actionInformation = await db.insert(req.body);
+        if (!req.body) respondWithError(res, INPUT_ERROR);
+        res.status(201).json(actionInformation);
+    } catch (e) {
+        respondWithError(res);
+    };
+});
+
+// delete 
+actionsRoute.delete('/:id', async (req, res) => {
+    try {
+        const actionInformation = await db.remove(req.params.id);
+        if (actionInformation === 0) respondWithError(res, NOT_FOUND_ERROR);
+        res.status(200).json(actionInformation);
+    } catch (e) {
+        respondWithError(res);
+    };
+});
