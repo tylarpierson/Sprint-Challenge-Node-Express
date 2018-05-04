fabf6c7bacd5e073e258e6519409c37c9947d57a
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-05-04 10:07:41 -0700

    working on fixing error to show get works

--- Routes/projectRoute.js
+++ Routes/projectRoute.js
@@ -1,2 +1,38 @@
-import Router from 'express';
+import express from 'express';
 import db from '../data/helpers/projectModel';
+import { validateBody, respondWithError } from '../utils';
+import { NOT_FOUND_ERROR, INPUT_ERROR, REMOVE_ERROR, PUT_ERROR } from '../Errors';
+
+const projectRoute = express.Router();
+
+// gets
+projectRoute.get('/', async (req, res) => {
+    try {
+        const projects = await db.get();
+        res.json(projects);
+    } catch (e) {
+        respondWithError(res);
+    }
+});
+
+projectRoute.get('/:id', async (req, res) => {
+    try {
+         const project = await db.get(req.params.id);
+         console.log(project);
+         if (!project.length) respondWithError(res, NOT_FOUND_ERROR);
+         res.json(project);
+    } catch (e) {
+        respondWithError(res);
+    };     
+});
+
+projectRoute.get('/:id/actions', async (req, res) => {
+    try {
+         const project = await db.getProjectActions(req.params.id);
+         console.log(actions);
+         if (!project.length) respondWithError(res, NOT_FOUND_ERROR);
+         res.json(project);
+    } catch (e) {
+        respondWithError(res);
+    };
+});
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
-import mainRouter from './Routes';
+const morgan = require('morgan');
+const mainRouter = './Routes';
 
 const server = express();
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+server.use(morgan('dev'));
 server.use(helmet());
 server.use(express.json());
 
